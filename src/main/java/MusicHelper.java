541875df3dc5ab61e023540ea00b4d24275d4fb9
Sandy Lao <70775435+sandyl289@users.noreply.github.com>
2022-11-29 17:02:05 -0500

    Squashed commit of the following:
    commit fe57373f61141ca105b6942a144a6152c6df48ea
    Merge: 0f73604 26f172b
    Author: Aidan Jackson <64502681+AidanJack@users.noreply.github.com>
    Date:   Tue Nov 29 14:15:00 2022 -0500
    
        Merge pull request #3 from sandyl289/feature/PlayerJump
    
        Added Jumping Mechaninc with Atached Sound
    
    commit 26f172bf29d328c7ad7f6d94a62d99e72c86f4a3
    Author: Aidan Jackson <aidan.jackson@mail.mcgill.ca>
    Date:   Tue Nov 29 13:54:19 2022 -0500
    
        Added Jumping Mechaninc with Atached Sound

--- src/main/java/MusicHelper.java
+++ src/main/java/MusicHelper.java
@@ -5,14 +5,14 @@ import javax.sound.sampled.FloatControl;
 import java.io.File;
 
 public class MusicHelper {
-    private final String BGM_PATH = "src/files/music_zapsplat_christmas_funk.wav"; //Sound from Zapsplat.com
-    private final String SCORE_SOUND_PATH = "src/files/zapsplat_multimedia_beep_bright_87460.wav"; //Sound from Zapsplat.com
-    private final String JUMP_SOUND_PATH = "src/files/zapsplat_multimedia_game_sound_short_beep_earn_point_pick_up_item_002_78374.wav"; //Sound from Zapsplat.com
-    private final String DEAD_SOUND_PATH = "src/files/zapsplat_cartoon_voice_high_pitched_says_ouch_001_15792.wav"; //Sound from Zapsplat.com
-    private final String GAME_OVER_PATH = "src/files/zapsplat_human_male_voice_says_game_over_001_15726.wav"; //Sound from Zapsplat.com
+    private static final String BGM_PATH = "src/files/music_zapsplat_christmas_funk.wav"; //Sound from Zapsplat.com
+    private static final String SCORE_SOUND_PATH = "src/files/zapsplat_multimedia_beep_bright_87460.wav"; //Sound from Zapsplat.com
+    private static final String JUMP_SOUND_PATH = "src/files/zapsplat_multimedia_game_sound_short_beep_earn_point_pick_up_item_002_78374.wav"; //Sound from Zapsplat.com
+    private static final String DEAD_SOUND_PATH = "src/files/zapsplat_cartoon_voice_high_pitched_says_ouch_001_15792.wav"; //Sound from Zapsplat.com
+    private static final String GAME_OVER_PATH = "src/files/zapsplat_human_male_voice_says_game_over_001_15726.wav"; //Sound from Zapsplat.com
 
-    void playBackgroundMusic() {
-        File file = new File(BGM_PATH);
+    static void playBackgroundMusic() {
+        File file = new File(MusicHelper.BGM_PATH);
         try {
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
             Clip clip = AudioSystem.getClip();
@@ -30,7 +30,7 @@ public class MusicHelper {
      * Plays sounds.
      * @param option: int (0 = SCORE sound; 1 = JUMP sound; 2 = DEAD sound; other = GAME OVER sound
      */
-    void playSound(int option){
+    static void playSound(int option){
         File file;
 
         if(option == 0){
--- src/main/java/PhysicsEngine.java
+++ src/main/java/PhysicsEngine.java
@@ -1,5 +1,12 @@
 public class PhysicsEngine {
 
+    private static final float AMPLITUDE = 7f;
+    private static final float STRETCH = 1.2f;
+    public static double moveWithForce(double radians, boolean jumping){
+        if (!jumping) return 0;
+        double yPos = AMPLITUDE * Math.cos(STRETCH * radians);
+        return yPos;
+    }
 
     public boolean detectCollision(int x1, int y1, int w1, int h1, int x2, int y2, int w2, int h2){
         //Check if any points from first object are in second object
