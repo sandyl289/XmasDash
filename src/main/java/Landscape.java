6eba29d198d444321e4c33de1f40e6e984e98fb3
Sandy Lao <sandy.lao@mail.mcgill.ca>
2022-11-30 22:28:41 -0500

    Simplify calculate next ground's position x

--- src/main/java/Landscape.java
+++ src/main/java/Landscape.java
@@ -6,7 +6,7 @@ public class Landscape {
     private ArrayList<Ground> grounds;
     private static BufferedImage GROUND_BUFFERED_IMG;
     private static BufferedImage MUD_BUFFERED_IMG;
-
+    
     Landscape() {
         ImageHelper imageHelper = new ImageHelper();
         this.GROUND_BUFFERED_IMG = imageHelper.getBufferedImg("src/files/ground.png");
@@ -15,18 +15,15 @@ public class Landscape {
        //Ground
         this.grounds = new ArrayList<>();
         int posX = 0;
-        for (int i = 0; i < 18; i++) {
+        for (int i = 0; i < 19; i++) {
             Ground ground = new Ground();
-            ground.setX(posX);
+            ground.setX(i*Ground.SIZE);
             this.grounds.add(ground);
-            posX += 50;
         }
     }
 
     public void paint(Graphics2D g2){
-
         g2.drawImage(this.MUD_BUFFERED_IMG, 0, Ground.Y +Ground.SIZE, 900, 200, null);
-
         for (Ground ground : this.grounds) {
             g2.drawImage(GROUND_BUFFERED_IMG, ground.getX(), Ground.Y, Ground.SIZE, Ground.SIZE, null);
         }
