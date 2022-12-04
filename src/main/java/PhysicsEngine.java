11569e02525ee4e9dcaa1069614bac4cf3b334da
Sandy Lao <sandy.lao@mail.mcgill.ca>
2022-12-04 18:16:53 -0500

    Remove unused nextPos function

--- src/main/java/PhysicsEngine.java
+++ src/main/java/PhysicsEngine.java
@@ -18,12 +18,4 @@ public class PhysicsEngine {
         //if ((y4-y3)*(x2-x1) - (x4-x3)*(y2-y1) == 0.0) return true;
         return false;
     }
-
-    public int[] nextPos(int x1, int y1, long cur_time, long prev_time){
-        int x2 = 0;
-        int y2 = 0;
-
-        int[] nextPos = {x2, y2};
-        return nextPos;
-    }
 }
