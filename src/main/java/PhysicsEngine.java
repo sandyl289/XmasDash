8188d294ee8ad89c723091b5a58a535c0336b121
Sandy Lao <sandy.lao@mail.mcgill.ca>
2022-12-04 20:50:09 -0500

    Refactoring + remove redundant local variable

--- src/main/java/PhysicsEngine.java
+++ src/main/java/PhysicsEngine.java
@@ -4,18 +4,13 @@ public class PhysicsEngine {
     private static final float STRETCH = 1.2f;
     public static double moveWithForce(double radians, boolean jumping){
         if (!jumping) return 0;
-        double yPos = AMPLITUDE * Math.cos(STRETCH * radians);
-        return yPos;
+        return AMPLITUDE * Math.cos(STRETCH * radians);
     }
 
     public static boolean detectCollision(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4){
-        float uA = ((x4-x3)*(y1-y3) - (y4-y3)*(x1-x3)) / ((y4-y3)*(x2-x1) - (x4-x3)*(y2-y1));
-        float uB = ((x2-x1)*(y1-y3) - (y2-y1)*(x1-x3)) / ((y4-y3)*(x2-x1) - (x4-x3)*(y2-y1));
-        
-        if (uA >= 0 && uA <= 1 && uB >= 0 && uB <= 1) {
-            return true;
-        }
-        //if ((y4-y3)*(x2-x1) - (x4-x3)*(y2-y1) == 0.0) return true;
-        return false;
+        final float v = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1);
+        float uA = ((x4-x3)*(y1-y3) - (y4-y3)*(x1-x3)) / v;
+        float uB = ((x2-x1)*(y1-y3) - (y2-y1)*(x1-x3)) / v;
+        return uA >= 0 && uA <= 1 && uB >= 0 && uB <= 1;
     }
 }
