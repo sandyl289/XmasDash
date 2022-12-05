a7fdb42321d9850deba8f8af36c9ffb798b9251c
Sandy Lao <sandy.lao@mail.mcgill.ca>
2022-12-04 23:33:05 -0500

    Move the array designators [] to the type.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 
 class Main{
-    public static void main(String args[]){
+    public static void main(String[] args){
         MusicHelper.playBackgroundMusic();
 
         JFrame window = new JFrame();
