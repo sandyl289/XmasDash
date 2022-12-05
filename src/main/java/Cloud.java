4b3f5934584715ec16eb740e3b41a623f556e340
Sandy Lao <sandy.lao@mail.mcgill.ca>
2022-12-05 14:08:15 -0500

    Move constructor up

--- src/main/java/Cloud.java
+++ src/main/java/Cloud.java
@@ -2,16 +2,17 @@ public class Cloud {
     private int x;
     private final int y;
     public static final int SIZE = 70;
+
+    Cloud(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
     public int getX() {
         return x;
     }
     public void setX(int x) {
         this.x = x;
-    }
-
-    Cloud(int x, int y){
-        this.x = x;
-        this.y = y;
     }
     public int getY() {
         return y;
